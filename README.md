ef96f3161c04d1f04d33c5ad99658fa9d98ecd90
Randy <drbugsmn@gmail.com>
2016-08-18 21:43:24 -0500

    Added basic mongo support (mongo needs to be running outside of this app).  Note that ZOnedDateTime is not supported by spring-data-mongo at this time.

--- README.md
+++ README.md
@@ -20,6 +20,35 @@ three major ones are:
 2. Mutability
 3. 
 
+## MongoDB
+### Mongo Config
+You will currently need to have a local mongo db running on the default port (unless you modify the application.yml).
+
+### SpringDataMongo  
+Note that ZonedDateTime is not supported by SpringData  https://jira.spring.io/browse/DATACMNS-834?jql=text%20~%20%22ZonedDateTime%22
+as mongo currently doesn't support it (there is a jira issue for mongo to support JSR310 objects).  An
+interesting observation in an early version of this code is that I persisted a zoned date time without
+errors, but when I did a findAll() it blew up.
+
+Document in mongo:
+
+    {
+         "_id" : ObjectId("57b66b08d985b73420332478"),
+         "_class" : "com.dood.java.dates.model.TimestampObject",
+         "localDate" : ISODate("2016-08-18T05:00:00Z"),
+         "localDateTime" : ISODate("2016-08-19T02:12:24.867Z"),
+         "zonedDateTime" : {
+             "dateTime" : ISODate("2016-08-19T02:12:24.867Z"),
+             "offset" : "-05:00",
+             "zone" : "America/Chicago"
+         }
+    }
+       
+Another interesting observation is that the localDate and LocalDateTIme objects is stored at zulu time (the above
+timestamps was created on that date, and we were GMT-5, thus 9:12:24 local time which is correct).  The
+localDate apparently stored the time as midnight (matches the spring data mongo docs for this type).
+
+
 ## Jackson Serialization
 ### Swagger
 There is a controller that implements swagger, go to http://localhost:8080 and it will redirect to the
--- src/main/java/com/dood/java/dates/controller/DateController.java
+++ src/main/java/com/dood/java/dates/controller/DateController.java
@@ -1,24 +1,52 @@
 package com.dood.java.dates.controller;
 
 import com.dood.java.dates.model.TimestampObject;
+import com.dood.java.dates.service.TimestampObjectService;
+import com.sun.istack.internal.NotNull;
 import io.swagger.annotations.Api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
+import java.util.List;
 
 @Api(description = "Provides various java 8 date operations")
 @RestController
 public class DateController {
     private static final Logger LOG = LoggerFactory.getLogger(DateController.class);
 
+    @Autowired
+    private TimestampObjectService timestampObjectService;
+
     @RequestMapping(value = "/getSome", method = RequestMethod.GET)
     public TimestampObject index() {
+        TimestampObject timestampObject = createTimestampObject();
+        LOG.debug("index called.  Object={}", timestampObject);
+        return timestampObject;
+    }
+
+    @RequestMapping(value = "/createSome", method = RequestMethod.POST)
+    public void createSomeRandom(@RequestParam @NotNull Integer number) {
+        for (int count = 0; count < number; count++) {
+            timestampObjectService.create(createTimestampObject());
+        }
+    }
+
+    @RequestMapping(value = "/getPersisted", method = RequestMethod.GET)
+    public List<TimestampObject> getPersisted() {
+        return timestampObjectService.findAll();
+    }
+
+
+    private TimestampObject createTimestampObject() {
+
         LocalDate localDate = LocalDate.now();
         LocalDateTime localDateTime = LocalDateTime.now();
         ZonedDateTime zonedDateTime = ZonedDateTime.now();
@@ -27,7 +55,7 @@ public class DateController {
                 .setZonedDateTime(zonedDateTime)
                 .setLocalDate(localDate)
                 .setLocalDateTime(localDateTime);
-        LOG.debug("index called.  Object={}", timestampObject);
+
         return timestampObject;
     }
 }
